9315d77bcfa642ea282d1aea34e173d85d353eb4
Margarita Lavruk <margolav27@yandex.ru>
2023-06-12 22:15:03 +0300

    выяснение БИН карты

--- main.py
+++ main.py
@@ -1,154 +1,35 @@
-import sys
-from time import time
+import argparse
+import json
+from useful_funcs import save_stat, search, luhn
+import logging
 
-from PyQt5 import QCoreApplication
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QLabel, QMessageBox,
-                             QProgressBar, QPushButton, QWidget)
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i','--init_path',default='files\\settings.json',help='Путь к json файлу с данными, default = files\\settings.json', action='store') 
+    group = parser.add_mutually_exclusive_group(required = True)
+    group.add_argument('-f','--find',help='Поиск номеров карт с заданным хэшем', action='store_true')
+    group.add_argument('-c','--check',help='Проверяет карту на достоверность', action='store_true')
+    group.add_argument('-s','--statistic',help='Вывод зависимости времени выполненя от кол-ва потоков', action='store_true')  
+    args = parser.parse_args()
+    init_path = args.init_path
+    try:
+        with open(init_path) as jf:
+            initial = json.load(jf)
+    except FileNotFoundError:
+        logging.error(f"{init_path} not found")
 
-from card_number_recover import recover_card_num
-from file_manager import FileManager
-from luhn_algorithm import is_card_number_valid
-from plot import draw_plot
+    mode = (args.find, args.check, args.statistic)
 
-SETTINGS_FILE = "files/settings.json"
-
-
-class AppGUI(QWidget):
-
-    def __init__(self, file_manager) -> None:
-        super().__init__()
-        self.initUI()
-        self.file_manager = file_manager
-
-    def initUI(self) -> None:
-        self.setWindowTitle("Card number finder application")
-        self.resize(300, 300)
-        self.pbar = QProgressBar(self)
-        self.pbar.setGeometry(70, 180, 200, 25)
-        self.pbar.setMaximum(100)
-        label = QLabel("Recover card number progessbar:", parent=self)
-        label.move(70, 160)
-        load_settings_btn = QPushButton("&Load settings", self)
-        load_settings_btn.setToolTip("Loads .json settings file")
-        load_settings_btn.clicked.connect(self.select_settings_file)
-        check_card_btn = QPushButton("&Check card number for validity", self)
-        check_card_btn.move(0, 30)
-        check_card_btn.clicked.connect(self.check_card_number)
-        recover_number_btn = QPushButton("&Recover bank card number", self)
-        recover_number_btn.move(0, 60)
-        recover_number_btn.clicked.connect(self.recover_number)
-        draw_plot_btn = QPushButton("&Draw statistics plot", self)
-        draw_plot_btn.move(0, 90)
-        draw_plot_btn.clicked.connect(self.draw_statistics_plot)
-        quit_btn = QPushButton("&Quit", self)
-        quit_btn.move(120, 270)
-        quit_btn.clicked.connect(QCoreApplication.instance().quit)
-
-    def select_settings_file(self) -> None:
-        """Method that initializes settings into a class File_manager
-        """
-        try:
-            path = QFileDialog.getOpenFileName(
-                self, "Select file", filter="*.json")[0]
-            done_msg = QMessageBox()
-            done_msg.setWindowTitle("Info message")
-            done_msg.setText(
-                f"Settings file successfully loaded from file {path}")
-            done_msg.setIcon(QMessageBox.Information)
-            done_msg.exec_()
-            self.file_manager.settings_path = path
-        except OSError:
-            self.file_manager.settings_path = SETTINGS_FILE
-            QMessageBox.information(
-                self, "Settings", f"Settings file was not loaded from file {path}." f"The default path was applied.\nPath: {SETTINGS_FILE}")
-
-    def check_card_number(self) -> None:
-        """Method that checks if the card number is valid
-        """
-        if not self.file_manager.are_settings_loaded:
-            msg = QMessageBox()
-            msg.setWindowTitle("Info message")
-            msg.setText("Settings were not loaded!")
-            msg.setIcon(QMessageBox.Critical)
-            msg.exec_()
-            return
-        if is_card_number_valid(self.file_manager.load_text(self.file_manager.card_number_path)):
-            done_msg = QMessageBox()
-            done_msg.setWindowTitle("Info message")
-            done_msg.setText("Bank card number is valid.")
-            done_msg.setIcon(QMessageBox.Information)
-            done_msg.exec_()
-        else:
-            done_msg = QMessageBox()
-            done_msg.setWindowTitle("Info message")
-            done_msg.setText("Bank card number is not valid.")
-            done_msg.setIcon(QMessageBox.Information)
-            done_msg.exec_()
-
-    def recover_number(self) -> None:
-        """The method that picks up the card number and calls the output method from the FileManager class
-        """
-        if not self.file_manager.are_settings_loaded:
-            msg = QMessageBox()
-            msg.setWindowTitle("Info message")
-            msg.setText("Settings were not loaded!")
-            msg.setIcon(QMessageBox.Critical)
-            msg.exec_()
-            return
-        self.pbar.setValue(0)
-
-        def update_progress(progress: int) -> None:
-            self.pbar.setValue(progress)
-        start = time()
-        card_number = recover_card_num(
-            self.file_manager.hash, self.file_manager.last_symbols, self.file_manager.bin, int(self.file_manager.number_of_processes), update_progress)
-        end = time()
-        self.pbar.setValue(0)
-        if (not card_number):
-            done_msg = QMessageBox()
-            done_msg.setWindowTitle("Info message")
-            done_msg.setText("Failed to find card number.")
-            done_msg.setIcon(QMessageBox.Information)
-            done_msg.exec_()
-        else:
-            validation_mark = is_card_number_valid(card_number)
-            self.file_manager.write_output(
-                f"{card_number} is {validation_mark}")
-            done_msg = QMessageBox()
-            done_msg.setWindowTitle("Info message")
-            done_msg.setText(
-                "Search completed successfully. The card number and its validity are written to the file.")
-            done_msg.setIcon(QMessageBox.Information)
-            done_msg.exec_()
-        self.file_manager.write_statistic(
-            end-start, self.file_manager.number_of_processes)
-        done_msg = QMessageBox()
-        done_msg.setWindowTitle("Info message")
-        done_msg.setText(
-            "An entry was successfully added to the statistics file.")
-        done_msg.setIcon(QMessageBox.Information)
-        done_msg.exec_()
-
-    def draw_statistics_plot(self):
-        if not self.file_manager.are_settings_loaded:
-            msg = QMessageBox()
-            msg.setWindowTitle("Info message")
-            msg.setText("Settings were not loaded!")
-            msg.setIcon(QMessageBox.Critical)
-            msg.exec_()
-            return
-        fig = draw_plot(self.file_manager.load_statistic())
-        self.file_manager.save_plot_image(fig)
-        done_msg = QMessageBox()
-        done_msg.setWindowTitle("Info message")
-        done_msg.setText("Plot image was saved successfully.")
-        done_msg.setIcon(QMessageBox.Information)
-        done_msg.exec_()
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    file_manager = FileManager()
-    window = AppGUI(file_manager)
-    window.show()
-    sys.exit(app.exec_())
+    match mode:
+        case (True, False, False):
+                logging.info('Поиск номера карточки\n')
+                search(initial, int(initial["processes_amount"]))
+        case (False, True, False):
+                logging.info('Проверка корректности карточки...')
+                luhn(initial)
+        case (False, False, True):
+                logging.info('Сбор данных...\n')
+                save_stat(initial)
+        case _:
+            logging.error("wrong mode")
---
+++ useful_funcs.py
@@ -0,0 +1,126 @@
+import hashlib
+import logging
+import json
+from matplotlib import pyplot as plt
+import multiprocessing as mp
+from functools import partial
+from time import time
+
+cores = mp.cpu_count()
+
+def serialisation_to_json(filename: str, key: bytes)->None:
+    '''
+    str сериализуется в json файл
+
+    Args:
+        filename (str): имя файла в который сериализуется str
+        key (bytes): объект, который сериализуется в json
+    '''
+    try:
+        with open(filename, "w") as f:
+            json.dump(list(key), f)
+    except FileNotFoundError:
+        logging.error(f"{filename} not found")
+
+def luhn(init: dict)->bool:
+    """
+    Проверяет номер на корректность алгоритмом Луна
+    args:
+        init(dict): входные данные
+    return:
+        (bool): True, если все сошлось, иначе - False
+    """
+    res = 0
+    try:
+        with open(init["found_card"]) as f:
+            data = json.load(f)
+    except FileNotFoundError:
+          logging.error(f"{init['found_card']} not found")
+    logging.info(data)
+    number = str(data["card_number"])
+    number = list(map(int, number))
+    if len(number) != 16:
+         logging.info("Номер не корректен")
+         data["luhn_check"] = "no result"
+    else:
+        last = number[15]
+        number.pop()
+        for n in number:
+            i = n * 2
+            if i > 9:
+                res += i % 10 + i // 10
+            else:
+                res += i
+
+        res = 10 - res % 10
+        logging.info(res)
+        if res == last:
+            logging.info("Карточка корректна")
+            data["luhn_check"] = "true"
+        else:
+            logging.info("Карточка не корректна")
+            data["luhn_check"] = "false"
+    logging.info(f"Результат сохранен по пути {init['found_card']}")
+    try:
+        with open(init["found_card"], 'w') as f:
+            json.dump(data, f)
+    except FileNotFoundError:
+          logging.error(f"{init['found_card']} not found")
+
+def search(initial: dict, processes: int)->None:
+    flag = 0
+    with mp.Pool(processes) as p:
+        for b in initial["first_digits"]:
+            logging.info(b)
+
+            for result in p.map(partial(checking_hash, int(b), initial), range(1000000)):
+                if result:
+                    logging.info('we have found ' + result + ' and have terminated pool')
+                    p.terminate()
+                    flag = 1
+                    logging.info('Найденная карта лежит по пути ' + initial["found_card"])
+                    data = {}
+                    data["card_number"] = f"{result}"
+                    data["luhn_check"] = "no result"
+                    try:
+                        with open(initial["found_card"], 'w') as f:
+                                json.dump(data, f)
+                    except FileNotFoundError:
+                        logging.error(f"{initial['found_card']} not found")
+                    break
+            if flag == 1:
+                break
+    if flag == 0:
+        logging.info('Карта не найдена')    
+
+def checking_hash(bin: int, initial:dict, number: int)->int:
+    """
+    Сравнивает хэш полученной карты с уже существующим
+
+    args:
+    number(int): сгенерированные цифры карты
+    return:
+    (int): номер, если хэш совпал, иначе False
+    """
+    if hashlib.sha3_224(f'{bin}{number:06d}{initial["last_digits"]}'.encode()).hexdigest() == initial["hash"]:
+        return f'{bin}{number:06d}{initial["last_digits"]}'
+    
+def save_stat(initial: dict):
+    """
+    Сохраняет зависимость времени поиска коллизии хэша от кол-ва процессов
+    args:
+        init(dict): входные данные
+    """
+    time_ = []
+    for i in range(int(initial["processes_amount"])):
+            start = time()
+            logging.info(f'количество процессов: {i+1}\n')
+            search(initial, i+1)
+            time_.append(time()-start)
+    fig=plt.figure(figsize=(30, 17))
+    plt.ylabel('Время')
+    plt.xlabel('процессы')
+    plt.title('зависимость времени от кол-ва процессов')
+    plt.plot(list(x+1 for x in range(int(initial["processes_amount"]))),time_) 
+    plt.savefig(f'{initial["stat_path"]}')
+    logging.info(f'Зависимость времени от процессов сохранена по пути {initial["stat_path"]}\n')
